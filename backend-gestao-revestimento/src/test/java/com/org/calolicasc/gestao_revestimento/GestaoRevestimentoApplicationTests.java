868425e21af74eafdac36f2ea22676c20ecfdd56
DouglasBohmer <douglas.bohmer.senai@gmail.com>
2026-08-07 10:06:21 -0300

    Add files via upload

---
+++ backend-gestao-revestimento/src/test/java/com/org/calolicasc/gestao_revestimento/GestaoRevestimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.calolicasc.gestao_revestimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoRevestimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
